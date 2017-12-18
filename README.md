b146229415a42711acada19924bd8fd19e2ce970
Sara Lima <clima.sara@gmail.com>
2017-12-18 17:44:51 +0100

    okay

---
+++ README.md
@@ -0,0 +1,4 @@
+newProject
+==========
+
+A Symfony project created on December 18, 2017, 2:56 pm.
---
+++ src/WCS/CoavBundle/Controller/DefaultController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace WCS\CoavBundle\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction()
+    {
+        return $this->render('@WCSCoav/Default/index.html.twig');
+    }
+}
